c98c3c8a7aba9fd74b883006aa35de2d214d2fc4
Vitor Santos <vitor7_sn@hotmail.com>
2019-06-25 18:13:58 -0300

    Versão final sem lixo

--- TaBarato/FrontEnd/src/index.js
+++ TaBarato/FrontEnd/src/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import Routes from "./routes";
 import "./config/StatusBarConfig";
 
+console.disableYellowBox = true;
+
 const App = () => <Routes />;
 
 export default App;
