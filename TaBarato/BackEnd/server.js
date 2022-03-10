3c6a01264ec82357cf25fcf55af6d48bae8cdefa
vitor.nascimento <vitor.nascimento@objective.com.br>
2022-03-10 15:57:45 -0300

    ajustando conecçao com o mongo

--- TaBarato/BackEnd/server.js
+++ TaBarato/BackEnd/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 mongoose
   .connect(
-    'mongodb+srv://Wilson:152029@cluster0-fddx8.mongodb.net/node-api?retryWrites=true',
+    'mongodb+srv://vitor:533596Vsn.@cluster0.mmqlk.mongodb.net/tabarato?retryWrites=true&w=majority',
       // process.env.MONGO_URL, //variavel de ambiente
     {
       useNewUrlParser: true
